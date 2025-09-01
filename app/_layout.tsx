ec17f8fb024073057db71898af1ff3ead7f11bee
Goni Zahavy <goni1993@gmail.com>
2025-09-01 12:46:15 +0000

    feat: add swipe gesture navigation from chat to sessions screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ConnectionProvider } from '../src/contexts/ConnectionContext';
 
 export default function RootLayout() {
   return (
     <ConnectionProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
     </ConnectionProvider>
   );
 }
